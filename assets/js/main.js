eea138188647e3d609e7289937d0ac49a0754a46
Nevi38 <quocthangngoc14@gmail.com>
2024-05-29 08:02:06 +0700

    Add background video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
 // Turn audio
-document.getElementById('application').addEventListener('click', function() {
-  document.getElementById('sound').play();
+// document.getElementById('application').addEventListener('click', function() {
+//   document.getElementById('sound').play();
+// });
+
+// Show preview / slide 1
+const btnPlayReview = document.getElementById('btn-play');
+btnPlayReview.addEventListener('click', function() {
+  const previewVideo = document.getElementById('preview-video');
+  const defaultVideo = document.getElementById('js-video-default');
+
+  defaultVideo.pause();
+
+  previewVideo.style.display = 'block';
+  btnPlayReview.style.display = 'none';
+  document.getElementById('sound').pause();
+
+  previewVideo.play();
 });
